6ad886cd93274487315739b086166cc58842e324
Pratyush Chaudhary <pratyushbabita7859@gmail.com>
2026-05-22 11:41:21 +0530

    Generate Production Build

--- part3_Programming_a_server_with_NodeJS_and_Express/phonebook-backend/index.js
+++ part3_Programming_a_server_with_NodeJS_and_Express/phonebook-backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const morgan = require("morgan")
 const app = express();
-const cors = require('cors')
 
 app.use(express.json());
-app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('req-body', function (req, res) { return JSON.stringify(req.body) })
 
